9da0a877110d54e5149224d085b9dc77755b25e5
Patrick Cahill <patrickhcahill@gmail.com>
2023-07-02 20:16:25 +1000

    Updating

--- votesbySA1s.py
+++ votesbySA1s.py
@@ -1,20 +1,14 @@
 import geopandas as gpd
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import webbrowser
 
 # Read in the shapefile
 import os
-path =  os.path.dirname(os.path.realpath(__file__))
-
-
-
-
-sa1s = gpd.read_file(f'{path}/data/datasa1/2022-federal-election-votes-sa1.shp')
+# path =  os.path.dirname(os.path.realpath(__file__))
+path = os.getcwd()
+sa1s = gpd.read_file(f'{path}/datasa1/2022-federal-election-votes-sa1.shp')
 
 output = sa1s.explore(column="DivisionNm", cmap="tab20")
-outfile = f'{path}/data/datasa1/output.html'
+outfile = f'{path}/docs/index.html'
 output.save(outfile)
 
 webbrowser.open(outfile)
